7dc9b2884b430d7d6506ddde3991aba5f02cf459
Namtulla Najmidin <namtulla.n@gmail.com>
2019-10-07 23:45:36 +0200

    get data from parse server

--- app/src/main/java/com/example/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -19,16 +25,77 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText model;
     private EditText price;
     private Button saveButton;
+    private TextView txtData;
+    private Button allData;
+    private String allBil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        allData=findViewById(R.id.allData);
         brand=findViewById(R.id.brand);
         model=findViewById(R.id.model);
         price=findViewById(R.id.price);
         saveButton=findViewById(R.id.saveButton);
+        txtData=findViewById(R.id.txtData);
+        txtData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("Bil");
+//                parseQuery.getInBackground("aWRAG6aIAj", new GetCallback<ParseObject>() {
+//                    @Override
+//                    public void done(ParseObject object, ParseException e) {
+//                        if(object !=null && e == null){
+//                            txtData.setText(object.get("Brand")+" "+object.get("Model")+" "+object.get("Price"));
+//                        }
+//                    }
+//                });
+
+                ParseQuery<ParseObject> query=ParseQuery.getQuery("Bil");
+                query.getInBackground("l5bIPIq2Me", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+
+                        if(object != null && e == null){
+                            txtData.setText(object.get("Brand")+" "+object.get("Model")+" "+object.get("Price"));
+                        }
+                    }
+                });
+            }
+        });
+
+        allData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> queryAll=ParseQuery.getQuery("Bil");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        allBil="";
+                        if( e ==null){
+                            if(objects.size()>0){
+                                for(ParseObject p:objects) {
+
+                                    allBil+=p.get("Brand")+" "+p.get("Model")+" "+p.get("Price")+"\n";
+
+
+
+                                }
+                                txtData.setText(allBil);
+                                FancyToast.makeText(SignUpActivity.this,allBil.toString(), FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+
+
+                            }else {
+                                FancyToast.makeText(SignUpActivity.this,e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
+                            }
+                        }
+                    }
+                });
+            }
+        });
 
 
     }
